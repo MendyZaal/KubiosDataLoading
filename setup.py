f379f47cb397bab2330ac1dfbecf956ff588b12f
Mendy Zaal <mendyzaal@hotmail.com>
2020-04-20 13:22:03 +1200

    added documents

--- setup.py
+++ setup.py
@@ -1,28 +1,29 @@
 from distutils.core import setup
+
 setup(
-  name = 'KubiosDataLoader',
-  packages = ['KubiosDataLoader'],
-  version = '0.1',
-  license='gpl-3.0',
-  description = 'Importing text files from Kubios ECG data analysis',   # Give a short description about your library
-  author = 'Mendy Zaal',
-  author_email = 'mendyzaal@hotmail.com',
-  url = 'https://github.com/user/reponame',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
-  keywords = ['SOME', 'MEANINGFULL', 'KEYWORDS'],   # Keywords that define your package best
-  install_requires=[            # I get to this in a second
-          'validators',
-          'beautifulsoup4',
-      ],
-  classifiers=[
-    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
-    'Intended Audience :: Developers',      # Define that your audience are developers
-    'Topic :: Software Development :: Build Tools',
-    'License :: OSI Approved :: MIT License',   # Again, pick a license
-    'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
-    'Programming Language :: Python :: 3.4',
-    'Programming Language :: Python :: 3.5',
-    'Programming Language :: Python :: 3.6',
-    'Programming Language :: Python :: 3.7',
-  ],
+    name='KubiosDataLoader',
+    packages=['KubiosDataLoader'],
+    version='0.1',
+    license='gpl-3.0',
+    description='Importing text files from Kubios ECG data analysis',
+    author='Mendy Zaal',
+    author_email='mendyzaal@hotmail.com',
+    url='https://github.com/MendyZaal/KubiosDataLoading',
+    download_url='',  # I explain this later on
+    keywords=['Kubios', 'dataloader', 'data-analysis'],
+    install_requires=[
+        'pandas',
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+        'Intended Audience :: Science/Research',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
 )
